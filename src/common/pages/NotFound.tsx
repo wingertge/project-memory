0dc7fc092937624366692a7cf2938923c54f59b1
Genna Wingert <wingertge@gmail.com>
2019-04-24 17:38:54 +0200

    a

--- src/common/pages/NotFound.tsx
+++ src/common/pages/NotFound.tsx
@@ -2,6 +2,7 @@ import {createStyles, makeStyles} from "@material-ui/styles"
 import * as React from "react"
 import {useTranslation} from "react-i18next"
 import Heading from "../components/common/Heading"
+import NotFoundImg from "../assets/notfound.png"
 
 const useStyles = makeStyles(createStyles({
     image: {
@@ -16,7 +17,7 @@ export const NotFound = () => {
     return (
         <div>
             <Heading>{t("I'm sorry Dave, I'm afraid I can't let you do that.")}</Heading>
-            <img src={"/static/media/notfound.png"} alt={t("Disappointed Tapir")} className={classes.image}/>
+            <img src={NotFoundImg} alt={t("Disappointed Tapir")} className={classes.image}/>
         </div>
     )
 }
