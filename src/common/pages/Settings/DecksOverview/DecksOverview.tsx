eaff01365e482293a505ea1a1d2bade70c54e6b0
Genna Wingert <wingertge@gmail.com>
2019-05-14 13:46:04 +0200

    Refactor file structure to be more consistent, clean up unnecessary components

--- src/common/pages/Settings/DecksOverview/DecksOverview.tsx
+++ src/common/pages/Settings/DecksOverview/DecksOverview.tsx
@@ -1,52 +1,48 @@
-import {Button, Card, CardContent, Theme} from "@material-ui/core"
+import {Button, Card, CardContent, Theme, Typography} from "@material-ui/core"
 import {createStyles, makeStyles} from "@material-ui/styles"
 import * as React from "react"
 import {oc} from "ts-optchain"
 import {Deck, useShallowDecksQuery} from "../../../../generated/graphql"
 import NewDeckDark from "../../../assets/new_deck_dark.svg"
 import NewDeckLight from "../../../assets/new_deck_light.svg"
+import ApolloErrorBox from "../../../components/apollo/ApolloErrorBox"
 import {useDialog, useID} from "../../../hooks"
-import ApolloErrorBox from "../../common/ApolloErrorBox"
-import Heading from "../../common/Heading"
 import CreateDeckForm from "./CreateDeckForm"
 import DeckDisplay from "./DeckDisplay"
 import Color from "color"
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-        card: {
+    cardContent: {
+        marginBottom: theme.spacing(-1)
+    },
+    newDeck: {
+        width: 144,
+        height: 192,
+        backgroundImage: `url("${theme.palette.type === "light" ? NewDeckLight : NewDeckDark}")`,
+        backgroundRepeat: "round",
+        backgroundColor: "transparent",
+        borderRadius: "8pt",
+        border: "none",
+        "&:hover": {
+            backgroundColor: Color(theme.palette.text.primary).alpha(0.2).string()
         },
-        cardContent: {
-            marginBottom: theme.spacing(-1)
-        },
-        newDeck: {
-            width: 144,
-            height: 192,
-            backgroundImage: `url("${theme.palette.type === "light" ? NewDeckLight : NewDeckDark}")`,
-            backgroundRepeat: "round",
-            backgroundColor: "transparent",
-            borderRadius: "8pt",
-            border: "none",
-            "&:hover": {
-                backgroundColor: Color(theme.palette.text.primary).alpha(0.2).string()
-            },
-            margin: theme.spacing(0.5, 0, 0, 0),
-            [theme.breakpoints.up("sm")]: {
-                marginLeft: theme.spacing(1)
-            }
-        },
-        deckList: {
-            display: "flex",
-            flexDirection: "row",
-            padding: 0,
-            maxWidth: "100%",
-            overflow: "auto",
-            flexWrap: "wrap"
-        },
-        deckListItem: {
-            width: "inherit"
+        margin: theme.spacing(0.5, 0, 0, 0),
+        [theme.breakpoints.up("sm")]: {
+            marginLeft: theme.spacing(1)
         }
-    })
-)
+    },
+    deckList: {
+        display: "flex",
+        flexDirection: "row",
+        padding: 0,
+        maxWidth: "100%",
+        overflow: "auto",
+        flexWrap: "wrap"
+    },
+    deckListItem: {
+        width: "inherit"
+    }
+}))
 
 export const DecksOverview = () => {
     const classes = useStyles()
@@ -64,9 +60,9 @@ export const DecksOverview = () => {
     return (
         <>
             <Dialog />
-            <Card className={classes.card}>
+            <Card>
                 <CardContent className={classes.cardContent}>
-                    <Heading>Decks</Heading>
+                    <Typography variant="h5">Decks</Typography>
                     <div className={classes.deckList}>
                         {ownedDecks.map(deck => (
                             <DeckDisplay key={deck!.id} deck={deck} owned={true} subscribed={false}/>
