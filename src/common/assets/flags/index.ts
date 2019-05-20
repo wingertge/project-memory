26d4be57dc3aef057d8097e857256d2d71b1b4e4
Genna Wingert <wingertge@gmail.com>
2019-05-20 20:10:01 +0200

    Add flags for all languages supported at launch Migrate existing flags to svg

--- src/common/assets/flags/index.ts
+++ src/common/assets/flags/index.ts
@@ -1,10 +1,33 @@
 /* tslint:disable:object-literal-key-quotes */
-import deDE from "./de-DE.png"
-import enGB from "./en-GB.png"
-import enUS from "./en-US.png"
-import jaJP from "./ja-JP.png"
-import vi from "./vi.png"
-import koKR from "./ko-KR.png"
+import deDE from "./germany-flag-square.svg"
+import enGB from "./united-kingdom-flag-square.svg"
+import enUS from "./united-states-of-america-flag-square.svg"
+import jaJP from "./japan-flag-square.svg"
+import vi from "./vietnam-flag-square.svg"
+import koKR from "./south-korea-flag-square.svg"
+import zh from "./china-flag-square.svg"
+import es from "./spain-flag-square.svg"
+import fr from "./france-flag-square.svg"
+import it from "./italy-flag-square.svg"
+import ru from "./russia-flag-square.svg"
+import pt from "./brazil-flag-square.svg"
+import tr from "./turkey-flag-square.svg"
+import nl from "./netherlands-flag-square.svg"
+import sv from "./sweden-flag-square.svg"
+import el from "./greece-flag-square.svg"
+import ga from "./ireland-flag-square.svg"
+import pl from "./poland-flag-square.svg"
+import he from "./israel-flag-square.svg"
+import hi from "./india-flag-square.svg"
+import nb from "./norway-flag.svg"
+import da from "./denmark-flag-square.svg"
+import ro from "./romania-flag-square.svg"
+import cs from "./czech-republic-flag-square.svg"
+import sw from "./tanzania-flag-square.svg"
+import hu from "./hungary-flag-square.svg"
+import uk from "./ukraine-flag-square.svg"
+import id from "./indonesia-flag-square.svg"
+import ar from "./egypt-flag-square.svg"
 
 export const flags = {
     "de-DE": deDE,
@@ -12,7 +35,31 @@ export const flags = {
     "en-US": enUS,
     "ja-JP": jaJP,
     "vi": vi,
-    "ko-KR": koKR
+    "ko-KR": koKR,
+    "zh-CHT": zh,
+    "zh-CHS": zh,
+    es,
+    fr,
+    it,
+    ru,
+    pt,
+    tr,
+    nl,
+    sv,
+    el,
+    ga,
+    pl,
+    he,
+    hi,
+    nb,
+    da,
+    ro,
+    cs,
+    sw,
+    hu,
+    uk,
+    id,
+    ar
 }
 
 export default flags
